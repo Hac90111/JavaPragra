b6139ee73839f248c9c9705b82e1209874b0bc07
Hac90111 <chaudharihimanshu11@gmail.com>
2022-09-20 21:06:31 -0400

    Try, Catch and Finally, ExceptionHandling

--- src/MiddleChar.java
+++ src/MiddleChar.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class MiddleChar {
+public class MiddleChar {
+
+
+    public static void main(String[] args) {
+        System.out.println(middleChar("Hello sweeti ji, how are youdoing in these fine days!!!!"));
+       };
+
+    public static String middleChar(String str){
+        int position;
+        String substring;
+
+        if (str.length()%2==0){
+            position= str.length()/2;
+            substring= String.valueOf(str.charAt(position));
+        }else {
+            position= str.length()/2 -1;
+            substring= str.substring(position, position+2);
+        }
+        return substring;
+    }
 }
