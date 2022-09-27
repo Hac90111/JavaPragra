6ed9e1eecf563b4125de550706e97802abc4c65e
Hac90111 <chaudharihimanshu11@gmail.com>
2022-09-27 18:59:20 -0400

    Date Object and methods

--- src/cwh/collections/Set_cwh.java
+++ src/cwh/collections/Set_cwh.java
@@ -1,5 +1,6 @@
 package cwh.collections;
 
+import java.util.Date;
 import java.util.HashSet;
 
 public class Set_cwh {
@@ -11,6 +12,13 @@ public class Set_cwh {
         myHashSet.add(3);
         myHashSet.add(6);  // it won't create duplicate entries
         System.out.println(myHashSet);
+
+        //Date class
+
+        Date date= new Date(); // refer to the docs for formatting
+        System.out.println(date);
+        System.out.println(date.getTime());
+        System.out.println(date.getDate());  //deprecated
 
     }
 
